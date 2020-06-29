1e291640c25c2232ab876f2b62f0b0258dbada08
thebestph <63948516+thebestph@users.noreply.github.com>
2020-06-29 16:15:25 +0800

    update 0.0.27

---
+++ info.js
@@ -0,0 +1,21 @@
+// WEBSITE INFO
+
+var website = {
+    name: "TheBestPH's Website",
+    creator: "TheBestPH",
+    created: "June 10, 2020", 
+    hosted: "June 27, 2020",
+    version: "0.0.27" // Change this
+}
+
+// SCRIPTS
+
+document.getElementById("name").innerHTML = website.name;
+document.getElementById("creator").innerHTML = website.creator;
+document.getElementById("created").innerHTML = website.created;
+document.getElementById("hosted").innerHTML = website.hosted;
+document.getElementById("version").innerHTML = website.version;
+
+
+// END
+
