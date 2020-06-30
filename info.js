1afbc0f89eeb691db3503d7d40290c465f27ba07
thebestph <63948516+thebestph@users.noreply.github.com>
2020-06-30 10:56:06 +0800

    bug fix

--- info.js
+++ info.js
@@ -5,7 +5,7 @@ var website = {
     creator: "TheBestPH",
     created: "June 10, 2020", 
     hosted: "June 27, 2020",
-    version: "0.0.35" // Change this
+    version: "0.0.36" // Change this
 }
 
 // SCRIPTS
