ddc4bd22d93436c55b0c6944ea6c003350d955a4
thebestph <63948516+thebestph@users.noreply.github.com>
2020-06-30 11:07:37 +0800

    0.0.39

--- info.js
+++ info.js
@@ -5,7 +5,7 @@ var website = {
     creator: "TheBestPH",
     created: "June 10, 2020", 
     hosted: "June 27, 2020",
-    version: "0.0.38" // Change this
+    version: "0.0.39" // Change this
 }
 
 // SCRIPTS
